4dcf9bc19e0df6aee0f07fadafcfb3af571d14c4
Lismey Plasencia <lismeyplasencia@gmail.com>
2021-01-17 12:06:51 -0500

    updated js file

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 // Grabbing elements from the quiz.html page
 
-var question = document.querySelector("#question");
-var answerType= document.querySelector("#answerType");
-var startbutton = document.querySelector("start-button")
+
+var answerType= document.querySelector(".answerType");
+var choices = Array.from(document.querySelectorAll("#choice-text"));
+var scoreDi = document.querySelector('.scoreDispl')
+
 
 // Creating Questions and Choices 
 
-var question = [
+var currentQuestion ={};
+var availableQuestions = []
+var questionCounter = 0;
+var Score_Points = 0;
+var score = 0
+var acceptingAnswers = true
+
+var questions = [
 {
     question:"Commonly used data types Do Not Include: ",
     choice1: "String",
     choice2: "Booleans",
     choice3: "Alerts",
     choice4: "Numbers",
+    answer: 3,
 },
 
 {
@@ -21,6 +31,7 @@ var question = [
     choice2: "Curly brackets",
     choice3: "Parentheses",
     choice4: "Square brackets",
+    answer: 2
 },
 {
     question:"Arrays in Java Script can be used to store __________",
@@ -28,6 +39,7 @@ var question = [
     choice2: "Other Arrays",
     choice3: "Booleans",
     choice4: "All of the above ",
+    answer: 3
 },
 {
     question:"String values must be enclosed within ______ when being assigned to variables.",
@@ -35,6 +47,7 @@ var question = [
     choice2: "Curly brackets",
     choice3: "Parentheses",
     choice4: "Square brackets",
+    answer: 1
 },
 
 {
@@ -43,15 +56,97 @@ var question = [
     choice2: "Terminal/bash",
     choice3: "For loops",
     choice4: "console.log",
+    answer: 4
 },
 ]
-//
 
-    for (let i = 0; i < question.length; i++) {
-        console.log(question);
-        
-        
+var maxQuestions = 4
+
+ startQuiz = () => {
+    questionCounter=0;
+    availableQuestions = [...questions]
+
+    console.log(availableQuestions)
+    getNewQuestion();
+}
+
+ getNewQuestion = () => {
+
+    // when reached the max qty of questions end the Quiz
+    if (availableQuestions.length === 0 || questionCounter > maxQuestions)  {
+       localStorage.setItem('mostRecentScore',score)
+       return window.location.assign('/highscores.html'); 
     }
+questionCounter++;
+
+var questionsIndex= Math.floor(Math.random()* availableQuestions.length)
+currentQuestion = availableQuestions[questionsIndex]
+console.log(currentQuestion)
+console.log(currentQuestion.question)
+
+var questionsDis = document.querySelector(".questionDisplayed");
+questionsDis.innerText =currentQuestion.question
+
+
+choices.forEach(choices => {
+    var number = choices.dataset['number'];
+    console.log(number)
+    choices.innerText = currentQuestion['choice' + number]
+ 
+})
+
+availableQuestions.splice(questionsIndex, 1)
+acceptingAnswers = true
+} 
+
+choices.forEach(choices => {
+
+choices.addEventListener('click', function () {
+    if (acceptingAnswers) return
+    acceptingAnswers= false
+    var selectedChoice=e.target
+    var selectedAnswer = selectedChoice.dataset['number']
+    
+    var classToApply = acceptingAnswers == currentQuestion.answer
+    answerType.innerText = ("Wrong");
+
+    if (classToApply === 'correct') {
+        incrementScore(Score_Points)
+        console.log(Score_Points)
+        answerType.innerText = ("Correct");
+    }
+    
+    selectedChoice.parentElement.classList.add(classToApply)
+    setTimeout(() => {
+        
+        selectedChoice.parentElement.classList.remove(classToApply)
+        getNewQuestion();
+        
+    }, 1000)
+
+
+})
+
+})
+
+incrementScore = num => {
+  score =+num
+  scoreDi.innerText=score
+ 
+
+}
+
+ 
+startQuiz();
+
+
+
+
+
+
+
+
+   
 
 
 
